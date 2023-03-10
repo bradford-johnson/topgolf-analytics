bb217ebb854b0763e4c3a90e19b08a7b75c56f44
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2023-03-10 09:23:00 -0500

    Create quick-9.R

---
+++ scripts/quick-9.R
@@ -0,0 +1,30 @@
+# laod packages
+library(ggsankey)
+library(tidyverse)
+
+tg <- read_csv("topgolf-scores-quick9.csv")
+
+tg <- tg |>
+  select(red_missed, yellow_missed, green_missed)
+
+tg1 <- tg |>
+  make_long(red_missed, yellow_missed, green_missed)
+
+pl <- ggplot(tg1, aes(x = x,                        
+                     next_x = next_x,                                     
+                     node = node,
+                     next_node = next_node,        
+                     fill = factor(x),
+                     label = node)) +
+  geom_sankey(flow.alpha = 0.5,          #This Creates the transparency of your node 
+                      node.color = "black",
+                      show.legend = TRUE,
+              flow.fill = "gray60")  
+
+pl +
+  labs(title = "test",
+       fill = "Nodes") +
+  geom_sankey_text(size = 4, 
+                   color = "black",
+                   type = "sankey") +# This specifies the Label format for each node
+scale_fill_manual(values = c("green", "red", "yellow"))
