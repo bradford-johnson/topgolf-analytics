2f4f5496b993370a3c1437217501a5925a9cc17e
Ford Johnson <bradford.lewis.johnson@outlook.com>
2023-03-16 19:07:11 -0400

    create csv #5

--- scripts/05-coords-test.R
+++ scripts/05-coords-test.R
@@ -1,25 +1,7 @@
 library(ggplot2)
 library(ggforce)
 
-dat = read.table(text="        x           y     sizes
-                 1  1 .5
-                 1  1  .25
-                 3  1 .5
-                 3  1  .25
-                 5  1 .5
-                 5  1  .25
-                 2  3  .75
-                 2  3  .25
-                 4  3  .75
-                 4  3  .25
-                 3  5.25  1
-                 3  5.25  .25
-                 4  7.50  1
-                 4  7.50 .25
-                 2  9.25  1
-                 2  9.25  .25
-                 4  11.50  1
-                 4  11.50  .25", header=TRUE)
+dat <- readr::read_csv("data/targets.csv")
 
 # set.seed(194)
 # # generate a random vector of numbers between 1 and 6
@@ -28,7 +10,7 @@ dat = read.table(text="        x           y     sizes
 # df <- data.frame(x,y)
 
 ggplot(dat) + 
-  geom_circle(aes(x0=x, y0=y, r=sizes), color = "black") + coord_equal() + theme_classic() +
+  geom_circle(aes(x0=x, y0=y, r=size), color = "black") + coord_equal() + theme_classic() +
   geom_rect(aes(xmin = 1.2, xmax = 4.8, ymin = 13.5, ymax = 14), fill = NA,
             color = "black", linewidth = .3)
   # geom_density_2d_filled(data = df, aes(x,y), alpha = .5) +
